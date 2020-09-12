e42e7a211a9dfa4535c99e59769cebb459699eb6
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-09-12 18:00:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+buttons = document.querySelectorAll(".drum");
+
+for (let index = 0; index < buttons.length; index++) {
+  buttons[index].addEventListener("click", function () {
+    alert("I got clicked");
+  });
+}
