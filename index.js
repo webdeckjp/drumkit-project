f2306c9810584f2376de25d64723c85f03c761e6
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-09-13 04:00:08 +0900

    works
    now it works

--- index.js
+++ index.js
@@ -1,7 +1,83 @@
+//select drum button elements on page
 buttons = document.querySelectorAll(".drum");
 
+//attach event listener to drum buttons
 for (let index = 0; index < buttons.length; index++) {
   buttons[index].addEventListener("click", function () {
-    alert("I got clicked");
+    switch (this.innerHTML) {
+      case "w":
+        playSound("tom-1");
+        break;
+      case "a":
+        playSound("tom-2");
+        break;
+      case "s":
+        playSound("tom-3");
+        break;
+      case "d":
+        playSound("tom-4");
+        break;
+      case "j":
+        playSound("snare");
+        break;
+      case "k":
+        playSound("crash");
+        break;
+      case "l":
+        playSound("kick-bass");
+        break;
+      default:
+        console.log("Error");
+        break;
+    }
+    animateKey(this.innerHTML);
   });
+}
+
+//attach event listener to keyboard keys
+document.addEventListener("keydown", function (e) {
+  switch (e.key) {
+    case "w":
+      playSound("tom-1");
+      break;
+    case "a":
+      playSound("tom-2");
+      break;
+    case "s":
+      playSound("tom-3");
+      break;
+    case "d":
+      playSound("tom-4");
+      break;
+    case "j":
+      playSound("snare");
+      break;
+    case "k":
+      playSound("crash");
+      break;
+    case "l":
+      playSound("kick-bass");
+      break;
+    default:
+      console.log("Error");
+      break;
+  }
+
+  animateKey(e.key);
+});
+
+//play sound function, hoisted
+function playSound(drumKey) {
+  var audio = new Audio(`./sounds/${drumKey}.mp3`);
+  audio.play();
+}
+
+//animate key
+function animateKey(currentKey) {
+  //function
+  let animatedKey = document.querySelector(`.${currentKey}`);
+  animatedKey.classList.add("pressed");
+  setTimeout(function () {
+    animatedKey.classList.remove("pressed");
+  }, 100);
 }
